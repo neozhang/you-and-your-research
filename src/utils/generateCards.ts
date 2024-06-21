f668f62196a98c854401290d300744cf0744deda
Neo Zhang <neo.zhangning@gmail.com>
2024-06-21 08:11:17 +0800

    Cleaning

--- src/utils/generateCards.ts
+++ src/utils/generateCards.ts
@@ -15,7 +15,7 @@ export const generateCards = async (
 
 	const prompt = `You are an multilingual expert in generating notes from documents. \n
 					Please respond in the same language as the user input provided. \n
-					The notes will need to be in JSON array using the schema: [{id: id, title: "title", content: "content"}]. \n
+					Your output should be in the JSON format. \n
 					Remember, the language of the notes must match the language of the user input. \n
 					Generate notes from the following doc. Instructions: \n
 					1. The titles of the notes should be short phrases which can be distinguishable with explicit subject matters, and holistically present the main logic structure of the provided doc. \n
@@ -25,24 +25,6 @@ export const generateCards = async (
 					5. Include relevant images (use Markdown to link the images). \n
 					6. Notes should be information-rich. Only keep the most informative notes. Combine related notes into one note. \n`;
 
-	// const prompt2 = `You will be given some paragraphs of texts. Use the following instructions to process ALL the provided paragraphs. Remain the original language of the provided texts. \n
-	// 				1. Exclude the headings and promotional paragraphs. Include all the other paragraphs, especially the quoted paragraphs. \n
-	// 				2. Group paragraphs based on the similarity of meanings. \n
-	// 				3. Give each group a new $title$, which should be a short complete sentence. \n
-	// 				4. Give each group a $summary$. \n
-	// 				5. Select sentences ($selectedSentences$) from each paragraph group, which can represent the core meaning of the group. Use triple dots (...) to separate sentences. \n
-	// 				6. Output in JSON with the schema: \n
-
-	// 					[
-	// 						{
-	// 							id: a unique id,
-	// 							title: $title$,
-	// 							content: $summary$ + $selectedSentences$,
-	// 						},
-	// 						...
-	// 					]`;
-
-	// Create the OpenAI client
 	const openai = createOpenAI({
 		apiKey: openAIAPIKey,
 		baseURL: openAIAPIEndpoint,
